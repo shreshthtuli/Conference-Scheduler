1c90ae67ef3ab24d926fb3faa427b5433c8d6555
udit01 <uditjainiitd@gmail.com>
2018-08-27 21:36:58 +0530

    Done first K_max hill climbing

---
+++ local_search/k-choice-hill-climbing-random-restarts/Static-start-ordered-exploration/SessionOrganizer.cpp
@@ -0,0 +1,292 @@
+/* 
+ * File:   SessionOrganizer.cpp
+ * Author: Kapil Thakkar
+ * 
+ */
+
+
+#include "SessionOrganizer.h"
+#include "Util.h"
+#include <ctime>
+#include <iostream>
+
+#define K_def 4
+
+
+SessionOrganizer::SessionOrganizer ( )
+{
+    parallelTracks = 0;
+    papersInSession = 0;
+    sessionsInTrack = 0;
+    processingTimeInMinutes = 0;
+    tradeoffCoefficient = 1.0;
+}
+
+SessionOrganizer::SessionOrganizer ( string filename )
+{
+    global_max = 0;
+    readInInputFile ( filename );
+    conference = new Conference ( parallelTracks, sessionsInTrack, papersInSession );
+    best_conference = new Conference (parallelTracks, sessionsInTrack, papersInSession);
+    int counter = 0;
+    for ( int i = 0; i < best_conference->getSessionsInTrack ( ); i++ )
+    {
+        for ( int j = 0; j < best_conference->getParallelTracks ( ); j++ )
+        {
+            for ( int k = 0; k < best_conference->getPapersInSession ( ); k++ )
+            {
+                conference->setPaper ( j, i, k, counter++ );
+            }
+        }
+    }
+    conference->printConference();
+    cout << "Start score : " << scoreOrganization() << "\n";
+}
+
+void SessionOrganizer::organizePapers ( )
+{
+    double result;
+    while(currentTime - startTime < ((processingTimeInMinutes * 60) - 10)){
+        result = run();
+        if (result > global_max){
+            global_max = result;
+            copyConference();
+        }
+        currentTime = time(0);
+    }
+}
+
+void SessionOrganizer::copyConference(){
+
+    int paperId;
+    for ( int i = 0; i < best_conference->getSessionsInTrack ( ); i++ )
+    {
+        for ( int j = 0; j < best_conference->getParallelTracks ( ); j++ )
+        {
+            for ( int k = 0; k < best_conference->getPapersInSession ( ); k++ )
+            {
+                paperId = conference->getTrack(j).getSession(i).getPaper(k);
+                best_conference->setPaper ( j, i, k, paperId );
+            }
+        }
+    }
+}
+
+double SessionOrganizer::run ( )
+{
+    conference->shuffle();
+    double max = 0;
+    cout << "Shuffled conference \n";
+    conference->printConference();
+    cout << "Score : " << scoreOrganization() << "\n" ;
+    while(getSuccessor() && currentTime - startTime < ((processingTimeInMinutes * 60) - 10)){
+        conference->printConference();
+        cout << "Score : " << scoreOrganization() << "\n" ;
+    }
+    max = scoreOrganization();
+    return max;
+}
+
+void SessionOrganizer::readInInputFile ( string filename )
+{
+    vector<string> lines;
+    string line;
+    startTime = time(0);
+    ifstream myfile ( filename.c_str () );
+    if ( myfile.is_open ( ) )
+    {
+        while ( getline ( myfile, line ) )
+        {
+            //cout<<"Line read:"<<line<<endl;
+            lines.push_back ( line );
+        }
+        myfile.close ( );
+    }
+    else
+    {
+        cout << "Unable to open input file";
+        exit ( 0 );
+    }
+
+    if ( 6 > lines.size ( ) )
+    {
+        cout << "Not enough information given, check format of input file";
+        exit ( 0 );
+    }
+
+    processingTimeInMinutes = atof ( lines[0].c_str () );
+    papersInSession = atoi ( lines[1].c_str () );
+    parallelTracks = atoi ( lines[2].c_str () );
+    sessionsInTrack = atoi ( lines[3].c_str () );
+    tradeoffCoefficient = atof ( lines[4].c_str () );
+
+    int n = lines.size ( ) - 5;
+    double ** tempDistanceMatrix = new double*[n];
+    for ( int i = 0; i < n; ++i )
+    {
+        tempDistanceMatrix[i] = new double[n];
+    }
+
+
+    for ( int i = 0; i < n; i++ )
+    {
+        string tempLine = lines[ i + 5 ];
+        string elements[n];
+        splitString ( tempLine, " ", elements, n );
+
+        for ( int j = 0; j < n; j++ )
+        {
+            tempDistanceMatrix[i][j] = atof ( elements[j].c_str () );
+        }
+    }
+    distanceMatrix = tempDistanceMatrix;
+
+    int numberOfPapers = n;
+    int slots = parallelTracks * papersInSession*sessionsInTrack;
+    if ( slots != numberOfPapers )
+    {
+        cout << "More papers than slots available! slots:" << slots << " num papers:" << numberOfPapers << endl;
+        exit ( 0 );
+    }
+}
+
+double** SessionOrganizer::getDistanceMatrix ( )
+{
+    return distanceMatrix;
+}
+
+bool SessionOrganizer::getSuccessor()
+{
+    int count = 0; 
+    double glob_max = 0.0;
+    
+    // int curr_i, curr_j , curr_k;
+    int saved_l, saved_m, saved_n;
+    int flag = 0;
+
+    this->cur_score = scoreOrganization();
+    // Find first index
+    for ( int i = 0; i < conference->getSessionsInTrack ( ); i++ )
+    {
+        for ( int j = 0; j < conference->getParallelTracks ( ); j++ )
+        {
+            for ( int k = 0; k < conference->getPapersInSession ( ); k++ )
+            {
+                count = 0; glob_max = 0.0;
+                saved_l = i; saved_m = j; saved_n = k;
+                flag = 0;
+                // Find another index
+
+                for ( int l = i; l < conference->getSessionsInTrack(); l++)
+                {
+                    for ( int m = 0; m < conference->getParallelTracks(); m++)
+                    {
+                        if (l == i && m <= j){
+                            // Skip those of same or previous tracks for this session number
+                            continue; 
+                        }
+                        for (int n = 0; n < conference->getPapersInSession (); n++)
+                        {
+                            conference->swapPapers(j, i, k, m, l, n);
+                            this->new_score = scoreOrganization();
+
+                            if (this->new_score > this->cur_score){
+                                // If performance improved return true
+                                flag = 1;
+                                count++;
+                                if(this->new_score > glob_max){
+                                    glob_max = this->new_score;
+                                    saved_l = l; saved_m = m; saved_n = n;
+                                }
+                                //what if we don't get k max?
+                                if(count > K_def){
+                                    //restore config
+                                    conference->swapPapers(j, i, k, m, l, n);
+
+                                    //swap to the saved thing
+                                    conference->swapPapers(j, i, k, saved_m, saved_l, saved_n);
+                                    return true;
+                                }
+
+                                // //restore and continue
+                                // conference->swapPapers(j, i, k, m, l, n);
+                            }
+                            // else{
+                            // Swap back and continue in any case
+                            conference->swapPapers(j, i, k, m, l, n);
+                            // }
+                        }
+                    }
+                }
+
+                // irrespective of count , swap papers, if found 1 greater, works in cases with < K_def greater
+                if(flag == 1){
+                    conference->swapPapers(j, i, k, saved_m, saved_l, saved_n);
+                }
+            }
+        }
+    }
+
+    // No successor found with better score
+    return false;
+
+}
+
+void SessionOrganizer::printSessionOrganiser ( char * filename)
+{
+    best_conference->printConference ( filename);
+    conference = best_conference;
+    cout << "Best score : " << scoreOrganization() << " \n";
+}
+
+double SessionOrganizer::scoreOrganization ( )
+{
+    // Sum of pairwise similarities per session.
+    double score1 = 0.0;
+    for ( int i = 0; i < conference->getParallelTracks ( ); i++ )
+    {
+        Track tmpTrack = conference->getTrack ( i );
+        for ( int j = 0; j < tmpTrack.getNumberOfSessions ( ); j++ )
+        {
+            Session tmpSession = tmpTrack.getSession ( j );
+            for ( int k = 0; k < tmpSession.getNumberOfPapers ( ); k++ )
+            {
+                int index1 = tmpSession.getPaper ( k );
+                for ( int l = k + 1; l < tmpSession.getNumberOfPapers ( ); l++ )
+                {
+                    int index2 = tmpSession.getPaper ( l );
+                    score1 += 1 - distanceMatrix[index1][index2];
+                }
+            }
+        }
+    }
+
+    // Sum of distances for competing papers.
+    double score2 = 0.0;
+    for ( int i = 0; i < conference->getParallelTracks ( ); i++ )
+    {
+        Track tmpTrack1 = conference->getTrack ( i );
+        for ( int j = 0; j < tmpTrack1.getNumberOfSessions ( ); j++ )
+        {
+            Session tmpSession1 = tmpTrack1.getSession ( j );
+            for ( int k = 0; k < tmpSession1.getNumberOfPapers ( ); k++ )
+            {
+                int index1 = tmpSession1.getPaper ( k );
+
+                // Get competing papers.
+                for ( int l = i + 1; l < conference->getParallelTracks ( ); l++ )
+                {
+                    Track tmpTrack2 = conference->getTrack ( l );
+                    Session tmpSession2 = tmpTrack2.getSession ( j );
+                    for ( int m = 0; m < tmpSession2.getNumberOfPapers ( ); m++ )
+                    {
+                        int index2 = tmpSession2.getPaper ( m );
+                        score2 += distanceMatrix[index1][index2];
+                    }
+                }
+            }
+        }
+    }
+    double score = score1 + tradeoffCoefficient*score2;
+    return score;
+}
